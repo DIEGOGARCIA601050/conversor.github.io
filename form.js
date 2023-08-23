958eb69bf6f703e7f9abb7b8216bc00ca13f1f4e
DiegoGRoldán <ekrnal9@gmail.com>
2023-08-22 19:58:22 -0600

    Regresa la función de los formularios

---
+++ form.js
@@ -0,0 +1,9 @@
+document.querySelector('form')
+      .addEventListener('submit', e => {
+        e.preventDefault()
+        const data = Object.fromEntries(
+          new FormData(e.target)
+        )
+        console.info(JSON?.stringify(data))
+      })
+
