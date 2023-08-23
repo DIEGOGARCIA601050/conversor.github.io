c7458483f8f0ebd547278415e4f2a97ed0b388c2
DiegoGRoldán <ekrnal9@gmail.com>
2023-08-22 19:47:27 -0600

    Simplificación del proyecto

---
+++ DecimalaBinario.js
@@ -0,0 +1,28 @@
+let Botonbinario = document.getElementById("binario");
+Botonbinario.addEventListener("click", binario);
+let number;
+function binario() {
+  let number = prompt("Que número quieres convertir a binario");
+  let n = number;
+  let binary = "";
+  while (number > 0) {
+    let remainder = number % 2;
+    //registra los resultados en binary como cadena de texto
+    binary = remainder + binary;
+    number = Math.floor(number / 2);
+  }
+  //lo muestra en pantalla
+  let results = document.getElementById("resultados");
+  let paragrapht = document.createElement("p");
+  paragrapht.innerHTML = "El número " + n + " a binario es: " + binary;
+  results.appendChild(paragrapht);
+
+  //de binario a decimal
+  let botonbinario = document.getElementById('-binario')
+  botonbinario.addEventListener('click', binarioinverso())
+  function binarioinverso() {
+    let number = prompt("Que número quieres convertir a binario");
+    let n = number;
+    parseInt(number, 2)
+  }
+}
---
+++ DolaresApesos.js
@@ -0,0 +1,29 @@
+      // Obtener el elemento del input de dólares
+      let divisa = document.getElementById('dollars').value
+      // Convertir el valor del input a un número entero
+      let num = parseInt(divisa);
+      // Guardar el valor del input en la variable n
+      let n = divisa;
+      // Obtener el botón de conversión
+      let convertidor = document.getElementById("convert-button");
+      // Agregar un evento de click al botón de conversión
+      convertidor.addEventListener("click", conversor);
+      // Función que se ejecuta al hacer clic en el botón de conversión
+      function conversor() {
+        // Llamar a la función de conversión
+        conversión();
+      }
+      // Función que realiza la conversión de dólares a pesos
+      function conversión() {
+        // Calcular el resultado de la conversión
+        let resultados = num * 17.15;
+        // Obtener el elemento donde se mostrarán los resultados
+        let results = document.getElementById("resultados");
+        // Crear un nuevo párrafo para mostrar el resultado de la conversión
+        let paragrapht = document.createElement("p");
+        // Asignar el contenido del párrafo con el resultado de la conversión
+        paragrapht.innerHTML = n + " dólares en pesos son: $" + resultados;
+        // Agregar el párrafo al elemento de resultados
+        results.appendChild(paragrapht);
+
+      }
