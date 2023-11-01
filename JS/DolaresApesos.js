f1d6ec777922b978b8d2ce94fc9fa85f63bd9db2
DiegoGRoldán <ekrnal9@gmail.com>
2023-11-01 14:26:51 -0600

    Hacemos más practica la función de convertir divisas

--- JS/DolaresApesos.js
+++ JS/DolaresApesos.js
@@ -7,12 +7,7 @@ let n = divisa;
 // Obtener el botón de conversión
 let convertidor = document.getElementById("convert-button");
 // Agregar un evento de click al botón de conversión
-convertidor.addEventListener("click", conversor);
-// Función que se ejecuta al hacer clic en el botón de conversión
-function conversor() {
-  // Llamar a la función de conversión
-  conversión();
-}
+convertidor.addEventListener("click", conversión);
 // Función que realiza la conversión de dólares a pesos
 function conversión() {
   // Calcular el resultado de la conversión
--- JS/form.js
+++ JS/form.js
@@ -1,4 +1,17 @@
 let Cantidad;
+const Convertir = ()=>{
+  let num = Cantidad.dollars
+  // Calcular el resultado de la conversión
+  let resultados = num * 17.15;
+  // Obtener el elemento donde se mostrarán los resultados
+  let results = document.getElementById("resultados");
+  // Crear un nuevo párrafo para mostrar el resultado de la conversión
+  let paragrapht = document.createElement("p");
+  // Asignar el contenido del párrafo con el resultado de la conversión
+  paragrapht.innerHTML = n + " dólares en pesos son: $" + resultados;
+  // Agregar el párrafo al elemento de resultados
+  results.appendChild(paragrapht);
+}
 document.querySelector('form')
       .addEventListener('submit', e => {
         e.preventDefault()
@@ -7,6 +20,6 @@ document.querySelector('form')
         )
          Cantidad = JSON?.stringify(data)
          Cantidad = JSON.parse(Cantidad)
+        Convertir()
         console.info(Cantidad)
-      })
-
+      });
