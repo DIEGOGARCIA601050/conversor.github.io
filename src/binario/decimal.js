3944b8ac96da5621fa9309ff57c89c2cd57226cf
DiegoGRoldán <123426763+DIEGOGARCIA601050@users.noreply.github.com>
2025-12-31 21:06:29 -0600

    Fix binary to decimal conversion output message

--- src/binario/decimal.js
+++ src/binario/decimal.js
@@ -1,9 +1,9 @@
 let results = document.getElementById("resultados");
 export function binarioinverso() {
     let numeroAconvertir = prompt("Que número binario quieres convertir a decimal");
-    let n = number;
-    number = parseInt(number, 2);
+    let numeroOriginal = numeroAconvertir;
+    numeroAconvertir = parseInt(numeroAconvertir, 2);
     let paragrapht = document.createElement("p");
-    paragrapht.innerHTML = `El numero ${n} en base 10 es ${number}`;
+    paragrapht.innerHTML = `El numero ${numeroOriginal} en base 10 es ${numeroAconvertir}`;
     results.appendChild(paragrapht);
 }
