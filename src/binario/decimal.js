881f48bef0cd5189b6e471bf7e39eab13225032a
DiegoGRoldán <123426763+DIEGOGARCIA601050@users.noreply.github.com>
2026-06-03 16:56:15 -0600

    Refactor binary to decimal conversion using mostrarEnPantalla

--- src/binario/decimal.js
+++ src/binario/decimal.js
@@ -1,9 +1,7 @@
-let results = document.getElementById("resultados");
+import { mostrarEnPantalla } from './mostrarEnPantalla.js';
 export function binarioinverso() {
     let numeroAconvertir = prompt("Que número binario quieres convertir a decimal");
     let numeroOriginal = numeroAconvertir;
     numeroAconvertir = parseInt(numeroAconvertir, 2);
-    let paragrapht = document.createElement("p");
-    paragrapht.innerHTML = `El numero ${numeroOriginal} en base 10 es ${numeroAconvertir}`;
-    results.appendChild(paragrapht);
+    mostrarEnPantalla(`El numero ${numeroOriginal} en base 10 es ${numeroAconvertir}`);
 }
