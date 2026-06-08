a0cea4e750c391952aa1988d5e7dae92088358e0
DiegoGRoldán <123426763+DIEGOGARCIA601050@users.noreply.github.com>
2026-06-07 18:28:21 -0600

    Validate binary input before conversion to decimal

--- src/binario/decimal.js
+++ src/binario/decimal.js
@@ -1,7 +1,15 @@
 import { mostrarEnPantalla } from './mostrarEnPantalla.js';
 export function binarioinverso() {
     let numeroAconvertir = prompt("Que número binario quieres convertir a decimal");
-    let numeroOriginal = numeroAconvertir;
-    numeroAconvertir = parseInt(numeroAconvertir, 2);
-    mostrarEnPantalla(`El numero ${numeroOriginal} en base 10 es ${numeroAconvertir}`);
+    const cadenaBinaria=/[01]+/
+    if(cadenaBinaria.test(numeroAconvertir))
+    {
+        let numeroOriginal = numeroAconvertir;
+        numeroAconvertir = parseInt(numeroAconvertir, 2);
+        mostrarEnPantalla(`El numero ${numeroOriginal} en base 10 es ${numeroAconvertir}`);
+    }
+    else
+    {
+        mostrarEnPantalla("Ingresa un numero válido");
+    }
 }
