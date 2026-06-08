f07beeb5190f9e1e16480b684dadba64d95108fc
DiegoGRoldán <123426763+DIEGOGARCIA601050@users.noreply.github.com>
2026-06-07 18:10:09 -0600

    Add input validation for binary conversion

--- src/binario/binario.js
+++ src/binario/binario.js
@@ -2,14 +2,20 @@ import { mostrarEnPantalla } from './mostrarEnPantalla.js';
 let results = document.getElementById("resultados");
 export function binario() {
     let number = parseFloat(prompt("Que número quieres convertir a binario"));
-    let numeroOriginal = number;
-    let binary = "";
-    while (number > 0) {
-        let remainder = number % 2;
-        //registra los resultados en binary como cadena de texto
-        binary = remainder + binary;
-        number = Math.floor(number / 2);
-    }
-    //lo muestra en pantalla
-    mostrarEnPantalla(`El numero ${numeroOriginal} en binario es ${binary}`);
+    if(!Number.isNaN(number))
+    {
+        let numeroOriginal = number;
+        let binary = "";
+        while (number > 0) {
+            let remainder = number % 2;
+            //registra los resultados en binary como cadena de texto
+            binary = remainder + binary;
+            number = Math.floor(number / 2);
+        }
+        //lo muestra en pantalla
+        mostrarEnPantalla(`El numero ${numeroOriginal} en binario es ${binary}`);
+    } else
+    {
+        mostrarEnPantalla("ingresa un numero válido");
+    }    
 }
